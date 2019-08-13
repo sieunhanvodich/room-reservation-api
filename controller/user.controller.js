f3e626e435d08e16c39794246fbf2758c14b6625
darkflamemaster2321998 <darkflamemaster2321998@gmail.com>
2019-08-13 17:11:03 +0700

    refs #1 login api

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,33 +2,42 @@ const user = require('../models/user.model');
 
 module.exports = {
     info: (req, res) => {
-        user.find({name: 'duong'}, function(error, user) {
+        user.find({ name: 'duong' }, function (error, user) {
             res.json(user);
         });
     },
-    login: (req, res) => {
-        // let username = 'duong.nguyentung@vti.com.vn';
-        // let password = '1234567';
-        // if (username == 'duong.nguyentung@vti.com.vn' && password == password) {
-        //     res.json({
-        //         a: 1,
-        //     })
-        // }
-        user.find({email: req.body.username}, (error, user) => {
-            if (error) {
-                console.log(error);
-                res.json({
-                    status: 1
-                });
-            };
-            if (req.body.password === user[0].password) {
-                
-                res.json({
-                    status: 2,
-                    name: user[0].name,
-                })
-            }
+    login: async (req, res) => {
+        // user.find({email: req.body.username}, (error, user) => {
+        //     if (error) {
+        //         console.log(error);
+        //         res.json({
+        //             status: 1
+        //         });
+        //     };
+        //     if (req.body.password === user[0].password) {
 
-        })
+        //         res.json({
+        //             status: 2,
+        //             name: user[0].name,
+        //         })
+        //     }
+
+        // })
+        try {
+            const email = req.body.username
+            const password = req.body.password
+            console.log(user.findByCredentials)
+            const user = await user.findByCredentials(email, password)
+            console.log(user)
+            if (!user) {
+                return res.status(401).send({ error: 'Login failed! Check authentication credentials' })
+            }
+            const token = await user.generateAuthToken()
+            console.log(token)
+            res.send({ user, token })
+        } catch (error) {
+            res.json({status: "Error"})
+            // res.status(400).send(error)
+        }
     }
 };
--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -8,11 +11,16 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        match: /^\S+@\S+\.\S+$/,
+        // match: /^\S+@\S+\.\S+$/,
         required: true,
         unique: true,
         trim: true,
         lowercase: true,
+        validate: value => {
+            if (!validator.isEmail(value)) {
+                throw new Error({error: 'Invalid Email address'})
+            }
+        }
     },
     phone: {
         type: String,
@@ -28,7 +36,47 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    }
+        minLength: 7,
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+userSchema.pre('save', async function (next) {
+    // Hash the password before saving the user model
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
+
+userSchema.methods.generateAuthToken = async function() {
+    // Generate an auth token for the user
+    const user = this
+    const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    // Search for a user by email and password.
+    console.log(email)
+    const user = await userSchema.find({ email: email} )
+    console.log(user)
+    // if (!user) {
+    //     throw new Error({ error: 'Invalid login credentials' })
+    // }
+    // const isPasswordMatch = await bcrypt.compare(password, user.password)
+    // if (!isPasswordMatch) {
+    //     throw new Error({ error: 'Invalid login credentials' })
+    // }
+    return user
+}
 
 module.exports = mongoose.model('user', userSchema);
